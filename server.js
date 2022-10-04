d4e859abab7ba47722427f62eef14921a4add28f
mrudulkolambe <mrudulkolambe02@gmail.com>
2022-10-04 11:19:07 +0530

    CORS error resolved

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express()
 require('dotenv').config()
 const bodyParser = require('body-parser')
+const cors = require('cors');
+ 
+app.use(cors())
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
