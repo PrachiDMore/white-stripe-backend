68c6995e2ef694170c8b3da32e61fc80c4d46599
mrudulkolambe <mrudulkolambe02@gmail.com>
2022-10-07 20:37:54 +0530

    Email template added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express()
+const moment = require('moment')
 require('dotenv').config()
 const bodyParser = require('body-parser')
 const cors = require('cors');
@@ -45,11 +46,87 @@ app.post('/self-contact', (req, res) => {
 })
 
 app.post('/self-appointment', (req, res) => {
+	let dateString = `${moment().format("DD-MMM-YY")}`
 	var mailOptions = {
 		from: process.env.SMTP_EMAIL,
 		to: process.env.SMTP_EMAIL,
 		subject: 'Appointment Request',
-		text: `${req.body.name} (${req.body.email}) has requested for appointment.`
+		text: `${req.body.name} (${req.body.email}) has requested for appointment.`,
+		html: `<!DOCTYPE html>
+		<html lang="en">
+		
+		<head>
+			<meta charset="UTF-8">
+			<meta http-equiv="X-UA-Compatible" content="IE=edge">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<title>Test</title>
+			<style>
+				@import url('https://fonts.googleapis.com/css2?family=Nunito:wght@200;300;400;500;600;700;800;900;1000&family=Raleway&display=swap');
+		
+				* {
+					padding: 0;
+					margin: 0;
+					word-wrap: break-word !important;
+				}
+		
+				.Raleway {
+					font-family: 'Raleway', sans-serif;
+				}
+		
+				.Nunito {
+					font-family: 'Nunito', sans-serif;
+				}
+				.flex-center{
+					display: flex;
+					justify-content: center;
+					align-items: center;
+				}
+				span{
+					line-height: 30px;
+					word-wrap: break-word;
+				}
+				.container{
+					width: 95%;
+				}
+			</style>
+		</head>
+		
+		<body>
+			
+			<div style="background-color: #192637; padding: 7px; color: white;">
+				<p class="Raleway" style="text-align: center;">Hello Team</p>
+				<h1 class="Nunito" style="text-align: center; margin-bottom: 10px;">New Appointment for ${req.body.name}</h1>
+				<p class="Raleway" style="margin-bottom: 10px; text-align: center;">Appointment Details</p>
+				<div class="flex-center">
+					<div style="background-color: #1D2C43; margin: auto;" class="container flex-center">
+						<div style="background-color: #1D2C43; padding: 10px; width: max-content;">
+							<div style="display: flex; align-items: center; width: max-content;"><span style="width: 130px; font-weight: bold;" class="Nunito">Name:
+								</span> &nbsp; <span class="Raleway">
+								${req.body.name}</span>
+							</div>
+							<div style="display: flex; align-items: center; width: max-content;"><span style="width: 130px; font-weight: bold;" class="Nunito">Mobile
+									Number: </span>
+								&nbsp; <span class="Raleway">${req.body.phonenumber}</span>
+							</div>
+							<div style="display: flex; align-items: center; width: max-content;"><span style="width: 130px; font-weight: bold;" class="Nunito">Email:
+								</span> &nbsp; <span class="Raleway">
+								${req.body.email}</span>
+							</div>
+							<div style="display: flex; align-items: center; width: max-content;"><span style="width: 130px; font-weight: bold;" class="Nunito">Branch:
+								</span> &nbsp; <span class="Raleway">
+								${req.body.location}</span>
+							</div>
+							<div style="display: flex; align-items: center; width: max-content;"><span style="width: 130px; font-weight: bold;" class="Nunito">Booked On:
+								</span> &nbsp;
+								<span class="Raleway">${dateString}</span>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</body>
+		
+		</html>`
 	};
 	transporter.sendMail(mailOptions, function (error, info) {
 		if (error) {
@@ -81,7 +158,8 @@ app.post('/user-appointment', (req, res) => {
 		from: process.env.SMTP_EMAIL,
 		to: req.body.email,
 		subject: 'Appointment request recorded',
-		text: `${req.body.name} your appointment request has been recorded. Our team will connect with you as soon as possible`
+		text: `${req.body.name} your appointment request has been recorded. Our team will connect with you as soon as possible`,
+		html: `<h1>hi</h1>`
 	};
 	transporter.sendMail(mailOptions, function (error, info) {
 		if (error) {
