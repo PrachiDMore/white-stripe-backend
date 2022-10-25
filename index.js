027ab604d23917d4dcfb499a33ef3497f921834f
PrachiDMore <94391314+PrachiDMore@users.noreply.github.com>
2022-10-25 23:12:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('dotenv').config()
 const bodyParser = require('body-parser')
 const cors = require('cors');
 app.use(cors({
-	origin: 'https://whitestripes-tailoring.com',
+	origin: 'https://white-stripe.vercel.app',
 }))
 
 app.use(bodyParser.urlencoded({ extended: false }))
